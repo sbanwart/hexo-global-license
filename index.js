dc079b04a42ad38b555b544e9897a1f3f3023ebf
Scott Banwart <scott@scottbanwart.com>
2017-07-18 16:23:47 -0400

    Add package.json. Add script to insert static HTML license.

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * hexo-global-license
+ * https://github.com/sbanwart/hexo-global-license.git
+ * Copyright (c) 2017, Scott Banwart
+ * Licensed under the MIT license.
+ */
+
+ 'use strict';
+
+ hexo.extend.helper.register('globalLicense', (config, page) => {
+     var license = '<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.';
+
+     return license;
+ });
