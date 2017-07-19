4e0da4fb62fa0167099d03db29b71d22a0cb8aef
Scott Banwart <scott@scottbanwart.com>
2017-07-18 21:10:58 -0400

    Add support for all permutations of the Creative Commons v4.0 license. Add support for reading license from configuration.

--- index.js
+++ index.js
@@ -7,8 +7,43 @@
 
  'use strict';
 
- hexo.extend.helper.register('globalLicense', (config, page) => {
-     var license = '<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.';
+ hexo.extend.helper.register('globalLicense', (config) => {
+     var license = ((licenseType, iconSize) => {
+         var icon = '';
+         switch (iconSize.toLowerCase()) {
+             case 'small':
+                 icon = '80x15';
+                 break;
+
+             case 'normal':
+                 icon = '88x31';
+                 break;
+            
+            default:
+                icon = '88x31';
+                break;
+         }
+
+         switch (licenseType) {
+             case 'by':
+                 return '<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/' + icon + '.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.';
+
+             case 'by-nd':
+                 return '<a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/' + icon + '.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">Creative Commons Attribution-NoDerivatives 4.0 International License</a>.';
+
+             case 'by-sa':
+                 return '<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/' + icon + '.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.';
+
+             case 'by-nc':
+                 return '<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/' + icon + '.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.';
+
+             case 'by-nc-nd':
+                 return '<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/' + icon + '.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.';
+
+             case 'by-nc-sa':
+                 return '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/' + icon + '.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.';
+         }
+     })(config.licenseType, config.licenseIconSize);
 
      return license;
  });
